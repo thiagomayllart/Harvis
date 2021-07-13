273b4e9de1de694a4b6aaa5857d14cf59fae977f
Thiago Mayllart <14459910+thiagomayllart@users.noreply.github.com>
2021-07-13 18:40:13 -0300

    Update c2_setup.py

--- c2_setup.py
+++ c2_setup.py
@@ -30,8 +30,8 @@ def setup_certificate(ssh,type):
     localcert = os.getcwd() + "/certificates/redirectors/"+type+"/cert.pem"
     localkey = os.getcwd() + "/certificates/redirectors/"+type+"/privkey.pem"
 
-    remote_path_cert = "/root/Mythic/C2_Profiles/HTTP/c2_code/cert.pem"
-    remote_path_key = "/root/Mythic/C2_Profiles/HTTP/c2_code/privkey.pem"
+    remote_path_cert = "/root/Mythic/C2_Profiles/http/c2_code/cert.pem"
+    remote_path_key = "/root/Mythic/C2_Profiles/http/c2_code/privkey.pem"
 
     sftp.put(localcert,remote_path_cert)
     sftp.put(localkey,remote_path_key)
@@ -41,7 +41,123 @@ def setup_certificate(ssh,type):
 
 def setup_api(ssh, ip,c2_type):
     if c2_type == 0:
-        setup_mythic_api(ssh,ip)
+        return setup_mythic_api(ssh,ip)
+
+def setup_listener(ip,type, c2_type):
+    if c2_type == 0:
+        setup_mythic_listener(ip,type)
+        
+        
+def get_password(ssh):
+    sftp = ssh.open_sftp()
+    env = sftp.open("Mythic/.env","r")
+    lines = env.readlines()
+    env.close()
+    sftp.close()
+    for i in lines:
+        if "MYTHIC_ADMIN_PASSWORD" in i:
+            password = i.split("=")[1]
+            return password.replace("\n")
+
+def setup_mythic_api(ssh,ip):
+    import requests
+    global api_key
+
+    password = get_password(ssh)
+    
+    url = "https://{1}:7443/login"
+    url = url.replace("{1}", ip)
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0",
+                     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                     "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Connection": "close",
+                     "Upgrade-Insecure-Requests": "1","Content-Type": "application/x-www-form-urlencoded"}
+    data = {"username": "mythic_admin", "password": password}
+    response = requests.post(url, headers=headers, data=data,verify=False)
+    access_token = response.cookies["access_token"]
+
+    url = "https://{1}:7443/api/v1.4/apitokens/"
+    url = url.replace("{1}", ip)
+    cookies = {
+        "access_token": access_token}
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0",
+                     "Accept": "*/*", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate",
+                     "content-type": "application/json",
+                     "Authorization": "Bearer "+access_token, "Connection": "close"}
+    json = {"token_type": "User"}
+    response = requests.post(url, headers=headers, cookies=cookies, json=json,verify=False)
+    api_key = response.json()["token_value"]
+
+    return password
+
+def setup_mythic_listener(ip,type):
+
+    url = "https://{1}:7443/api/v1.4/c2profiles"
+    url = url.replace("{1}", ip)
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0",
+                     "Accept": "*/*", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate",
+                     "apitoken": api_key,
+                     "Connection": "close"}
+    response = requests.get(url, headers=headers,verify=False)
+    text = response.json()
+    needed_profiles = {}
+    profile_config = config.c2_profiles[type]
+    for i in profile_config:
+        name = i["name"]
+        for j in text:
+            if j["name"] == name:
+                needed_profiles[j["id"]] = name
+
+    for i in needed_profiles:
+        name = needed_profiles[i]
+    
+        url = "https://{1}:7443/api/v1.4/c2profile_upload_file_webhook"
+        url = url.replace("{1}", ip)
+        headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0",
+                         "Accept": "*/*", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate",
+                         "content-type": "application/json",
+                         "apitoken": api_key}
+
+        code_raw = ""
+        for i in profile_config:
+            if i["name"] == name:
+                code_raw = i["config"]
+                break
+
+        code64 = base64.b64encode(code_raw.encode("utf-8"))
+
+        json = {"input": {"data": code64.decode("utf-8"), "file_path": "config.json", "id": str(i)}}
+        requests.post(url, headers=headers, json=json,verify=False)
+
+    for i in needed_profiles:
+
+        url = "https://{1}:7443/api/v1.4/start_stop_profile_webhook"
+        url = url.replace("{1}",ip)
+        
+        headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0",
+                         "Accept": "*/*", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate",
+                         "content-type": "application/json",
+                         "apitoken": api_key,
+                         "Connection": "close"}
+        json={"input": {"action": "start", "id": str(i)}}
+        requests.post(url, headers=headers, json=json, verify=False)
+
+def firewall_rules(ssh):
+    (stdin, stdout, stderr) = ssh.exec_command("iptables -A INPUT -p tcp -s {IP_PROXY} --dport 7443 -j ACCEPT".replace("{IP_PROXY}", config.ip_allowed_to_connect_c2))
+    ssh_stdout = stdout.read()
+    ip = requests.get('https://api.ipify.org').text
+    (stdin, stdout, stderr) = ssh.exec_command("iptables -A INPUT -p tcp -s {IP_PROXY} --dport 7443 -j ACCEPT".replace("{IP_PROXY}", ip))
+    ssh_stdout = stdout.read()
+    (stdin, stdout, stderr) = ssh.exec_command("iptables -A INPUT -p tcp -s 127.0.0.1 --dport 7443 -j ACCEPT")
+    ssh_stdout = stdout.read()
+    (stdin, stdout, stderr) = ssh.exec_command("iptables -A INPUT -p tcp -s {IP_PROXY} --dport 8080 -j ACCEPT".replace("{IP_PROXY}", ip))
+    ssh_stdout = stdout.read()
+    (stdin, stdout, stderr) = ssh.exec_command(
+        "iptables -A INPUT -p tcp -s {IP_PROXY} --dport 7443 -j ACCEPT".replace("{IP_PROXY}", config.ip_allowed_to_connect_c2))
+    ssh_stdout = stdout.read()
+    (stdin, stdout, stderr) = ssh.exec_command("iptables -A INPUT -p tcp -s 0.0.0.0/0 --dport 7443 -j DROP")
+    ssh_stdout = stdout.read()
+    (stdin, stdout, stderr) = ssh.exec_command("iptables -A INPUT -p tcp -s 0.0.0.0/0 --dport 8080 -j DROP")
+    ssh_stdout = stdout.read()
 
 def setup_listener(ip,type, c2_type):
     if c2_type == 0:
